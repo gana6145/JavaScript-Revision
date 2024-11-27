85c7ee62da1783b2148801ffec21a0b7ce5a5c6b
Ganesh Jadhav <jadhav.ganesh1700@gmail.com>
2024-11-27 19:42:40 +0530

    this is all about var data type with different ..

---
+++ variables and data types/script.js
@@ -0,0 +1,11 @@
+var name ="Ganesh";
+console.log(name);
+
+var paymentmode;//variable declared but not assigned value
+console.log(paymentmode); // output is undefined;
+
+paymentmode="credit card"; //assigned the value
+console.log(paymentmode); //output credit card
+
+let isloggedin=true; // boolean data types true and false .
+console.log(isloggedin); //output true i.e boolean type
